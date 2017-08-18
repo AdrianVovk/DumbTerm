1cce11e5cda7d497a938897ede1b229d0b16ae25
Adrian Vovk <adrianvovk@gmail.com>
2017-08-17 22:18:24 -0400

    Improve update mechanism

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ if [ `tty | grep -o -P '\d'` = $1 ]; then
    read -srn1 # Pause for input
 fi
 echo "---------------------- Installing DumbTerm ----------------------"
-sudo cp ./DumbTerm.sh /opt/DumbTerm.sh
-echo -n "Update Location: "; echo "`pwd`" | sudo tee /opt/DumbTermInstall
+sudo ln -s `pwd`/DumbTerm.sh /opt/DumbTerm.sh
 sudo mkdir -p /etc/systemd/system/getty@tty$1.service.d
 sudo cp ./Systemd.conf /etc/systemd/system/getty@tty$1.service.d/override.conf
 sudo systemctl daemon-reload
