45632c623bd05cc2547c8a7d4585306cada2dd06
Adrian Vovk <adrianvovk@gmail.com>
2017-08-17 22:05:19 -0400

    Add logout warning

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [ `tty | grep -o -P '\d'` = $1 ]; then
+   echo "[WARN] It seems like you are trying to install this tool to /dev/tty$1, but you are currently using it."
+   echo "[WARN] Continuing with this installation will log you out of this terminal session. Press any key to continue or press ^C to cancel"
+   read -srn1 # Pause for input
+fi
 echo "---------------------- Installing DumbTerm ----------------------"
 sudo cp ./DumbTerm.sh /opt/DumbTerm.sh
 echo -n "Update Location: "; echo "`pwd`" | sudo tee /opt/DumbTermInstall
