8833c10d414448eb2a0857add85013878d1e8e58
Adrian Vovk <adrianvovk@gmail.com>
2017-08-17 21:50:32 -0400

    Fix spacing

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "---------------------- Installing DumbTerm ----------------------"
 sudo cp ./DumbTerm.sh /opt/DumbTerm.sh
-echo -n "Update Location:"; echo "`pwd`" | sudo tee /opt/DumbTermInstall
+echo -n "Update Location: "; echo "`pwd`" | sudo tee /opt/DumbTermInstall
 sudo mkdir -p /etc/systemd/system/getty@tty$1.service.d
 sudo cp ./Systemd.conf /etc/systemd/system/getty@tty$1.service.d/override.conf
 sudo systemctl daemon-reload
